e249b54acc8ff3ca552ffc957ba4cb6082540c95
Jeroen Braspenning <mail@Jeroenbraspenning.nl>
2016-11-26 12:51:58 +0100

    documentation

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 ## Based on https://github.com/Wodby/docker4wordpress
-phpmyadmin localhost:8001
 Developed on Wordpress v.4.6.1 
-
-
-NOtes
-- Wordpress page edit templates list is based on user level, and set in functions.php
 
 ## Development
 gulp develop
 
-## Deploy
-gulp upload-dev
+## Tools
+PhpMyAdmin: http://localhost:8001
+Webserver: http://locahost:8000
 
-## Requirement
+## Requirements
 - Docker
 - Docker Compose
 - Npm
@@ -22,9 +18,7 @@ gulp upload-dev
 - Clone this repo
 - Download the latest worpdress and paste the contents in this folder, beware to not overwrite the wp-content folder
 - docker-compose up
-- import the databse using localhost:8001
+- import the databse using localhost:8001 ( phpmyadmin )
 
 ### Backup database
 docker-compose exec mariadb sh -c 'exec mysqldump --all-databases -uroot -p"root-password"' > databases.sql
-
-
--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,6 @@ var config = JSON.parse(fs.readFileSync('./gulp.json'))
 var themePath = 'wp-content/themes/' + config.theme
 var distPath = themePath + '/dist'
 var assetPath = themePath + '/assets'
-console.log(themePath)
 
 var path = {
   src_css: assetPath + '/scss/',
@@ -42,48 +41,6 @@ var path = {
   dist_img: distPath + '/img/'
 
 }
-
-// var hosts = {
-//   local: {
-//     folder: 'wp-content/',
-//     database: {
-//       host: 'http://jeroens-macbook-pro.local/',
-//       port: '3306',
-//       name: 'wordpress',
-//       user: 'root',
-//       password: 'password'
-//     },
-//     wordpress: {
-//       db_prefix: 'wp_',
-//       domain: 'jeroens-macbook-pro.local:8080',
-//       folder: 'a'
-//     }
-//   }
-// }
-
-/*
-#
-PROJECT_ROOT_FOLDER="/home/server/public_html/clients/gandh/website/"
-PROJECT_FOLDER="${PROJECT_ROOT_FOLDER}dev/wp-content/"
-DEPLOY_FOLDER="${PROJECT_ROOT_FOLDER}_deploy/"
-
-# SERVER
-SERVER_DOMAIN="www.jeroenbraspenning.nl"
-SERVER_FOLDER="clients/gandh/website/dev"
-SERVER_HOST="jeroenbraspenning.nl"
-SERVER_USER="server"
-
-# DB
-DB_HOST="localhost"
-DB_NAME="server_gandh"
-DB_USER="server_db"
-DB_PASSWORD="7jlnY1sg"
-
-# WORDPRESS
-WP_PREFIX="wp_dev_"
- */
-
-// var revision = Math.floor(Date.now() / 1000)
 
 /* Helpers */
 gulp.task('clean', function () {
@@ -137,33 +94,6 @@ gulp.task('bump-major', function () {
     .pipe(gulp.dest('./'))
 })
 
-// gulp.task('upload-dev', shell.task([
-//   'rsync -av --progress --delete --exclude "uploads"  --exclude ".git"  --exclude "docker-runtime"  --exclude "node_modules"  --exclude "w3tc-config" --exclude "cache" --exclude "advanced-cache.php" ' + hosts.local.folder + ' ' + hosts.dev.user + '@' + hosts.dev.host + ':' + hosts.dev.root + hosts.dev.folder
-// ]))
-
-// gulp.task('upload-prod', shell.task([
-//   'rsync -av --progress --exclude "uploads" --exclude "plugins/daph-press/archives" --exclude "w3tc-config" --exclude "cache" --exclude "advanced-cache.php" ' + hosts.local.folder + ' ' + hosts.prod.user + '@' + hosts.prod.host + ':' + hosts.prod.root + hosts.prod.folder
-// ]))
-
-// gulp.task('migrate-db-dev', shell.task([
-//   './scripts/migrate-db.sh --target-user ' + hosts.dev.user +
-//   ' --target-root-folder ' + hosts.dev.root +
-//   ' --db-host ' + hosts.local.database.host +
-//   ' --db-port ' + hosts.local.database.port +
-//   ' --db-user ' + hosts.local.database.user +
-//   ' --db-password ' + hosts.local.database.password +
-//   ' --db-name ' + hosts.local.database.name +
-//   ' --db-target-host ' + hosts.dev.database.host +
-//   ' --db-target-user ' + hosts.dev.database.user +
-//   ' --db-target-password ' + hosts.dev.database.password +
-//   ' --db-target-name ' + hosts.dev.database.name +
-//   ' --source-host ' + hosts.local.wordpress.domain +
-//   ' --target-host ' + hosts.dev.wordpress.domain +
-//   ' --source-wp-folder ' + hosts.local.wordpress.folder +
-//   ' --target-wp-folder ' + hosts.dev.wordpress.folder +
-//   ' --source-wp-db-prefix ' + hosts.local.wordpress.db_prefix +
-//   ' --target-wp-db-prefix ' + hosts.dev.wordpress.db_prefix
-// ]))
 
 /* Docker */
 
@@ -290,10 +220,6 @@ gulp.task(
     gulp.watch([ path.src_img + '**/*.*' ], ['process-images'])
     gulp.watch([ path.src_twig + '**/*.twig' ], ['compile-templates'])
     gulp.watch([ path.src_js + 'vendor/**/*.js' ], ['js-concat-vendor'])
-
-  // gulp.watch(assetPath + '/fonts/**/*',      ['compile-fonts'])
-  // gulp.watch(assetPath + '/images/**/*',     ['compile-images'])
-  // gulp.watch(assetPath + '/templates/**/*',  ['compile-templates'])
   }
 )
 
@@ -314,154 +240,4 @@ gulp.task('deploy', function (callback) {
   )
 })
 
-// gulp.task('deploy-dev', function (callback) {
-//   runSequence(
-//     'clean',
-//     ['compile-templates', 'process-images', 'compile-styles', 'copy-fonts', 'js-compile-app', 'js-concat-vendor'],
-//     'upload-dev',
-//     // 'migrate-db-dev',
-//     callback
-//   )
-// })
 
-// gulp.task('deploy-prod', function (callback) {
-//   runSequence(
-//     'clean',
-//     ['copy-fonts', 'process-images', 'compile-templates', 'compile-styles', 'js-compile-app', 'js-concat-vendor'],
-//     'js-optimize',
-//     'upload-prod',
-//     callback
-//   )
-// })
-
-// 
-// 
-// gulp.task('deploy-assets', function (callback) {
-//   runSequence(
-//     'clean',
-//     ['compile-styles', 'compile-scripts', 'compile-images', 'compile-fonts', 'compile-templates'],
-//     ['optimize-styles', 'optimize-scripts', 'optimize-images'],
-//     'version-assets',
-//     ['replace-versisonned-assets-in-assets', 'replace-versisonned-assets-in-templates'],
-//     'gzip-assets',
-//     'publish-to-s3',
-//     callback
-//   )
-// })
-
-// // Scripts
-// // -------
-
-// gulp.task('compile-scripts', function () {
-//   return (
-//     gulp.src([
-//       assetPath + '/js/script.js'
-//     ])
-//     .pipe(concat('script.js'))
-//     .pipe(gulp.dest(distPath + '/js'))
-//   )
-// })
-
-// gulp.task('optimize-scripts', function () {
-//   return gulp.src(distPath + '/js/script.js')
-//     .pipe(uglify())
-//     .pipe(gulp.dest(distPath + '/js/'))
-// })
-
-// // Images
-// // -------
-
-// gulp.task('compile-images', function () {
-//   return gulp.src(assetPath + '/images/**/*')
-//     .pipe(gulp.dest(distPath + '/images'))
-// })
-
-// gulp.task('optimize-images', function () {
-//   return gulp.src(distPath + '/images/**/*')
-//     .pipe(imagemin({
-//       progressive: true,
-//       svgoPlugins: [{removeViewBox: false}],
-//       use: [pngquant()]
-//     }))
-//     .pipe(gulp.dest(distPath + '/images'))
-// })
-
-// // Fonts
-// // -----
-
-// gulp.task('compile-fonts', function () {
-//   return gulp.src(assetPath + '/fonts/**/*')
-//     .pipe(gulp.dest(distPath + '/fonts'))
-// })
-
-// // Templates
-// // ---------
-
-// gulp.task('compile-templates', function () {
-//   return gulp.src(assetPath + '/templates/**/*')
-//     .pipe(gulp.dest(themePath))
-// })
-
-// // Versionning
-// // -----------
-
-// gulp.task('version-assets', function () {
-//   return gulp.src(distPath + '/**/*')
-//     .pipe(rev())
-//     .pipe(gulp.dest(distPath))
-//     .pipe(rev.manifest())
-//     .pipe(gulp.dest(themePath))
-// })
-
-// gulp.task('replace-versionned-assets-in-assets', function () {
-//   return gulp.src([
-//       themePath + '/**/*.json',
-//       distPath + '/**/*.css',
-//       distPath + '/**/*.js'
-//     ])
-//     .pipe(collect({
-//       replaceReved: true,
-//       dirReplacements: {
-//         '/wp-content/themes/visible/dist': config.productionAssetURL
-//       }
-//     }))
-//     .pipe(gulp.dest(distPath))
-// })
-
-// gulp.task('replace-versisonned-assets-in-templates', function () {
-//   return gulp.src([
-//       themePath + '/**/*.json',
-//       themePath + '/*.php'
-//     ])
-//     .pipe(collect({
-//       replaceReved: true,
-//       dirReplacements: {
-//         '/wp-content/themes/visible/dist': config.productionAssetURL
-//       }
-//     }))
-//     .pipe(gulp.dest(themePath))
-// })
-
-// // S3
-// // --
-
-// gulp.task('gzip-assets', function () {
-//   return gulp.src([
-//       '!' + distPath + '/**/*.gz',
-//       distPath + '/**/*'
-//     ])
-//     .pipe(awspublish.gzip({ ext: '.gz' }))
-//     .pipe(gulp.dest(distPath))
-// })
-
-// gulp.task('publish-to-s3', function () {
-//   var publisher = awspublish.create(config.aws)
-//   var headers = {
-//     'Cache-Control': 'max-age=31536000, no-transform, public'
-//   }
-
-//   return gulp.src(distPath + '/**')
-//     .pipe(publisher.publish(headers))
-//     .pipe(publisher.sync())
-//     .pipe(awspublish.reporter())
-// })
